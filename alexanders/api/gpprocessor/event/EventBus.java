89306b1cc56eae8455e955c9d940dcd4f0fa0da3
Alexander Stekelenburg <anoniem.axander@gmail.com>
2017-05-05 23:35:28 +0200

    Make EventBus store handlers per mod

--- alexanders/api/gpprocessor/event/EventBus.java
+++ alexanders/api/gpprocessor/event/EventBus.java
@@ -1,13 +1,12 @@
 package alexanders.api.gpprocessor.event;
 
-import java.util.List;
 import java.util.Map;
 
 public abstract class EventBus
 {
-    protected Map<Class<? extends GPPEvent>, List<Object>> eventHandlerMap;
+    protected Map<Class<? extends GPPEvent>, Map<String, Object>> eventHandlerMap;
 
-    public abstract void register(Object eventHandler);
+    public abstract void register(String pluginID, Object eventHandler);
 
     public abstract void fireEvent(GPPEvent event);
 
