8024d53cf8f956f5246e21abb1be45a5263820c7
Alexander Stekelenburg <anoniem.axander@gmail.com>
2017-05-05 23:37:46 +0200

    Make IPCEvent extend GPPEvent

--- alexanders/api/gpprocessor/event/IPCEvent.java
+++ alexanders/api/gpprocessor/event/IPCEvent.java
@@ -2,7 +2,7 @@ package alexanders.api.gpprocessor.event;
 
 import alexanders.api.gpprocessor.plugin.IPCMessage;
 
-public class IPCEvent
+public class IPCEvent extends GPPEvent
 {
     private IPCMessage message;
     private String sender;
