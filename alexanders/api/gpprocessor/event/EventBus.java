701c1348f73a9f3fc255cf76b781ddeb83106aae
Alexander Stekelenburg <anoniem.axander@gmail.com>
2017-05-06 12:30:25 +0200

    Refine EventBus and GPPEvent API

--- alexanders/api/gpprocessor/event/EventBus.java
+++ alexanders/api/gpprocessor/event/EventBus.java
@@ -1,15 +1,18 @@
 package alexanders.api.gpprocessor.event;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Map;
 
 public abstract class EventBus
 {
-    protected Map<Class<? extends GPPEvent>, Map<String, Object>> eventHandlerMap;
+    protected Map<String, Map<String, ArrayList<Method>>> eventHandlerMap; // event, Map<pluginID, eventHandlerMethods>
 
     public abstract void register(String pluginID, Object eventHandler);
 
     public abstract void register(String pluginID, Method eventHandlerMethod);
+
+    protected abstract void register(String pluginID, Method eventHandlerMethod, String event);
 
     public abstract void fireEvent(GPPEvent event);
 
--- alexanders/api/gpprocessor/event/GPPEvent.java
+++ alexanders/api/gpprocessor/event/GPPEvent.java
@@ -2,6 +2,10 @@ package alexanders.api.gpprocessor.event;
 
 public class GPPEvent
 {
+    public GPPEvent() // Always allow empty constructor for type checking
+    {
+    }
+
     public String getType()
     {
         return getClass().getSimpleName();
