b674f4e8b63eaf1de8133508d184794c298df108
Alexander Stekelenburg <anoniem.axander@gmail.com>
2017-05-07 21:18:07 +0200

    Added Container utility class

---
+++ alexanders/api/gpprocessor/Container.java
@@ -0,0 +1,15 @@
+package alexanders.api.gpprocessor;
+
+public class Container<V>
+{
+    public V value;
+
+    public Container()
+    {
+    }
+
+    public Container(V value)
+    {
+        this.value = value;
+    }
+}
